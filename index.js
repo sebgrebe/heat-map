3f268a438f97b19fdc5c328ee8796354b163f9d8
sebgrebe <sebbecker@gmx.net>
2017-09-18 21:42:50 +0100

    fix x-axis label issue

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
                                    const padding_bottom = 80;
                                    const padding_right = 130;
                                    const w = 1000;
-                                   const h = window.innerHeight-140;
+                                   const h = 500;
                                    const graph = document.getElementById('graph');
                                    const svg = d3.select(graph)
                                                         .append('svg')
